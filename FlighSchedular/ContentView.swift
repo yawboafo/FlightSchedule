7fb585407e507fc12a1ec3ad73e595742e6f7a15
yaw koranteng boafo <koranteng@smsgh.com>
2019-09-30 12:27:59 +0000

    Initial Commit

---
+++ FlighSchedular/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlighSchedular
+//
+//  Created by Engineer 144 on 30/09/2019.
+//  Copyright © 2019 Engineer 144. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
